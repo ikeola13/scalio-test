c9d18b53ac5e8ab1a5269436c85d5de80d9239a4
ikeola emmanuel <emmanuel.ikeola@student.griffith.ie>
2021-05-13 22:52:33 +0100

    added redirect file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ResultsComponent } from './results/results.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SearchComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'results', component: ResultsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
